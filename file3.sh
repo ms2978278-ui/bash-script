882ec3ada80cef1abef33e4fc7e6c5a592f8f3dc
ms2978278-ui <ms2978278@gmail.com>
2026-05-06 19:18:50 +0300

    Create file3.sh
    Added first bash script

---
+++ file3.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+read -p "Please put your name: " name 
+
+if [ "$name" = "mohammed" ]
+then
+     echo "hello Mr Root"
+
+elif [ "$name" = "saeed" ]
+then
+     echo "hello DAD"
+
+else
+     echo "Go Out"
+
+fi
